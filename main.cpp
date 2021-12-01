6c14eb87efddac8113e7c4fcd0efb2a0f4797181
S5201967 <91668770+S5201967@users.noreply.github.com>
2021-12-01 23:41:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+bool numeriprimi(int i, int n){
+    if(n%i==0)  {
+        return false;
+    }
+    if((i+1) ==  n) {
+        return true;
+    }
+    return  numeriprimi(i+1,n);
+}
 
 int main(){
-  return 0;
+    int num;
+    bool val;
+    cout<<"inserisci numero intero: ";cin>>num;
+    val=numeriprimi(2,num);
+    if(val) {
+        cout<<"Numero primo"<<endl;
+    } else {
+        cout<<"Numero non primo"<<endl;
+    }
+    return 0;
 }
+
+
+
