6697737e9ef7f37a0a1684eefd6b66ae5dec74f8
S5201967 <91668770+S5201967@users.noreply.github.com>
2021-12-01 23:43:14 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+bool numeriprimi(int i, int n){
+    if(n%i==0)  {
+        return false;
+    }
+    if((i+1) ==  n) {
+        return true;
+    }
+    return  numeriprimi(i+1,n);
+}
+
 
